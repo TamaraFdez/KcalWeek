7cb56d641068154a670b5a252eff0334c6922b9a
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-06-03 02:10:28 +0200

    feat: add user data input and calorie estimation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,24 @@ function App() {
       ]
     }
   );
-  const [weeMeals, setWeekMeals] = useState(
+  const [weekMeals, setWeekMeals] = useState(
     () => JSON.parse(localStorage.getItem("weekMeals")) || diasSemana
   );
   const [draggedFoodId, setDraggedFoodId] = useState(null);
 
   const [nuevaComida, setNuevaComida] = useState({name: "", kcal: 0, protein: 0, carbs: 0});
-
+  const [usuario, setUsuario] = useState(() => {
+    const saved = localStorage.getItem("usuario");
+    return saved 
+    ? JSON.parse(saved)
+    : {
+      peso: "70",
+      altura: "175",
+      edad: "30",
+      sexo: "hombre",
+      actividad: "1.55", //moderado
+    }
+  })
 
   const agregarComida = () => {
     const nueva = {
@@ -45,8 +56,9 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem("foodItems", JSON.stringify(foodItems));
-    localStorage.setItem("weekMeals", JSON.stringify(weeMeals));
-  }, [foodItems, weeMeals]);
+    localStorage.setItem("weekMeals", JSON.stringify(weekMeals));
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  }, [foodItems, weekMeals, usuario]);
 
   const handleDragStart = (id) => {
     setDraggedFoodId(id);
@@ -97,26 +109,28 @@ function App() {
   const borrarComida = (id) => {
     setFoodItems((prev) => prev.filter((item) => item.id !== id));
   }
+
+  const p = parseFloat(usuario.peso);
+  const a = parseFloat(usuario.altura);
+  const e = parseInt(usuario.edad);
+  const tbm = usuario.sexo === "hombre" 
+    ? 10 * p + 6.25 * a - 5 * e + 5
+    : 10 * p + 6.25 * a - 5 * e - 161;
+  const actividad = parseFloat(usuario.actividad);
+  const gastoDiario = Math.round(tbm * actividad);
+  const definicion = Math.round(gastoDiario * 0.8);
+  const volumen = Math.round(gastoDiario * 1.15);
+
   return (
     
     <div className="App">
       <h1>Planificador de Comidas</h1>
-      <div className="add-food-form">
-        <h2>Añadir nueva comida</h2>
-        <input type="text"
-        placeholder="Pollo"
-        value={nuevaComida.name} onChange={(e) => setNuevaComida({ ...nuevaComida, name: e.target.value})} />
-        <input type="number"
-        placeholder="Kcal/100g"
-        value={nuevaComida.kcal} onChange={(e) => setNuevaComida({ ...nuevaComida, kcal: e.target.value})} />
-        <input type="number"
-        placeholder="Proteina/100g"
-        value={nuevaComida.protein} onChange={(e) => setNuevaComida({ ...nuevaComida, protein: e.target.value})} />
-        <input type="number"
-        placeholder="Carbohidratos/100g"
-        value={nuevaComida.carbs} onChange={(e) => setNuevaComida({ ...nuevaComida, carbs: e.target.value})} />
-        <button onClick={agregarComida}>Añadir</button>
-      </div>
+      <div className="calorie-info">
+          <h3>Calorías estimadas:</h3>
+          <p>Gasto: Diario {gastoDiario} kcal / Semanal {gastoDiario * 7} kcal</p>
+          <p>Definición: {definicion} kcal / Semanal {definicion * 7} kcal</p>
+          <p>Volumen: {volumen} kcal / Semanal {volumen * 7} kcal</p>
+          </div>
       <div className="food-list">
         <h2>Lista de Comidas</h2>
         <ul className="food-items">
@@ -137,7 +151,7 @@ function App() {
 
       <div className="week-grid">
         {diasSemana.map((dia) => {
-          const comidas = weeMeals[dia] || [];
+          const comidas = weekMeals[dia] || [];
           const totales = calcularTotales(comidas || []); ;
           return (
             <div
@@ -169,6 +183,71 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div className="add-food-form">
+        <h2>Añadir nueva comida</h2>
+        <label> Comida:
+        <input type="text"
+        placeholder="Pollo"
+        value={nuevaComida.name} onChange={(e) => setNuevaComida({ ...nuevaComida, name: e.target.value})} />
+        </label>
+        <label> Kcal:
+        <input type="number"
+        placeholder="Kcal/100g"
+        value={nuevaComida.kcal} onChange={(e) => setNuevaComida({ ...nuevaComida, kcal: e.target.value})} />
+        </label>
+        <label>Proteínas:
+        <input type="number"
+        placeholder="Proteina/100g"
+        value={nuevaComida.protein} onChange={(e) => setNuevaComida({ ...nuevaComida, protein: e.target.value})} />
+         </label>
+         <label>Carbohidratos:
+        <input type="number"
+        placeholder="Carbohidratos/100g"
+        value={nuevaComida.carbs} onChange={(e) => setNuevaComida({ ...nuevaComida, carbs: e.target.value})} />
+        </label>
+        <button onClick={agregarComida}>Añadir</button>
+      </div>
+      <div className="user-form">
+        <h2>Datos del usuario</h2>
+        <label>
+          Peso en Kg:
+          <input type="number"
+          value={usuario.peso}
+          onChange={(e)=> setUsuario((u) => ({...u, peso: e.target.value}))} />
+
+        </label>
+        <label>
+          Altura en cm:
+          <input type="number"
+          value={usuario.altura}
+          onChange={(e)=> setUsuario((u) => ({...u, altura: e.target.value}))} />
+        </label>
+        <label>
+          Edad:
+          <input type="number"
+          value={usuario.edad}
+          onChange={(e)=> setUsuario((u) => ({...u, edad: e.target.value}))} />
+        </label>
+        <label>
+          Sexo:
+          <select value={usuario.sexo} onChange={(e) => setUsuario((u) => ({...u, sexo: e.target.value}))}>
+            <option value="hombre">Hombre</option>
+            <option value="mujer">Mujer</option>
+          </select>
+        </label>
+        <label>
+          Actividad:
+          <select value={usuario.actividad} onChange={(e) => setUsuario((u) => ({...u, actividad: e.target.value}))}>
+            <option value="1.2">Sedentario</option>
+            <option value="1.375">Ligero</option>
+            <option value="1.55">Moderado</option>
+            <option value="1.725">Intenso</option>
+            <option value="1.9">Muy intenso</option>
+          </select>
+        </label>
+       
+          
       </div>
     </div>
     
